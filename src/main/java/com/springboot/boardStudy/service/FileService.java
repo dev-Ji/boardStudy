5f7c3dfc1d71bc551850fee7b8aa7b96cf171a24
dev-Ji <wendyyi@naver.com>
2021-04-29 14:32:42 +0900

    2021.04.29

---
+++ src/main/java/com/springboot/boardStudy/service/FileService.java
@@ -0,0 +1,35 @@
+package com.springboot.boardStudy.service;
+
+import com.springboot.boardStudy.domain.entity.File;
+import com.springboot.boardStudy.domain.repository.FileRepository;
+import com.springboot.boardStudy.dto.FileDto;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class FileService {
+    private FileRepository fileRepository;
+
+    public FileService(FileRepository fileRepository){
+        this.fileRepository = fileRepository;
+    }
+
+    @Transactional
+    public Long saveFile(FileDto fileDto) {
+        return fileRepository.save(fileDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public FileDto getFileDto(Long id){
+        File file = fileRepository.findById(id).get();
+
+        FileDto fileDto = FileDto.builder()
+                .id(id)
+                .origFilename(file.getOrigFilename())
+                .filename(file.getFilename())
+                .filePath(file.getFilePath())
+                .build();
+        return fileDto;
+    }
+}
